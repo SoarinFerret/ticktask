270b5cf42802384c1ee1446bddbb554f91c94438
Soarinferret <thecodye@kanto.cloud>
2025-01-03 16:38:55 -0600

    multi-select close tasks

--- cmd/close.go
+++ cmd/close.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"errors"
 	"strconv"
+	"strings"
 
 	"github.com/KEINOS/go-todotxt/todo"
 	"github.com/pterm/pterm"
@@ -14,54 +16,76 @@ var closeCmd = &cobra.Command{
 	Use:     "close",
 	Aliases: []string{"done", "complete"},
 	Short:   "Mark a task as complete",
-	Long:    ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		id, err := cmd.Flags().GetInt("id")
-		if err != nil {
-			pExit("Error getting task ID:", err)
+	Args: func(cmd *cobra.Command, args []string) error {
+		// if int flag is set, no args are allowed
+		id, _ := cmd.Flags().GetInt("id")
+		if id != -1 && len(args) > 0 {
+			return errors.New("cannot use both ID flag and args")
 		}
 
-		if id == -1 && len(args) == 1 {
-			id, err = strconv.Atoi(args[0])
+		// all args must be integers
+		for _, arg := range args {
+			_, err := strconv.Atoi(arg)
 			if err != nil {
-				pExit("Invalid task ID:", err)
+				return errors.New("all args must be integers")
 			}
 		}
 
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+
+		ids := []int{}
+
+		id, _ := cmd.Flags().GetInt("id")
 		if id != -1 {
+			ids = append(ids, id)
+		}
+
+		for _, arg := range args {
+			id, _ := strconv.Atoi(arg)
+			ids = append(ids, id)
+		}
+
+		// if ids is empty, lets do it interactively
+		if len(ids) == 0 {
+			tasks, err := todotxt.GetTasks()
+			if err != nil {
+				pExit("Error loading task list: ", err)
+			}
+
+			// filter out completed tasks
+			tasks = tasks.Filter(todo.FilterNotCompleted)
+
+			var options []string
+			for _, task := range tasks {
+				options = append(options, strconv.Itoa(task.ID)+" | "+task.Todo)
+			}
+
+			selectedOptions, _ := pterm.DefaultInteractiveMultiselect.WithOptions(options).Show()
+
+			// convert selectedOptions to ids by parsing the number before the first space
+			for _, option := range selectedOptions {
+				id, _ := strconv.Atoi(option[:strings.Index(option, " ")])
+				ids = append(ids, id)
+			}
+
+		}
+
+		for _, id := range ids {
 			task, err := todotxt.CompleteTask(id)
 			if err != nil {
 				pExit("Error completing task:", err)
 			}
 
-			// commit to git
-			err = git.CommitTodo()
-
-			if err != nil {
-				pExit("Error committing task completion: ", err)
-			}
-
 			pterm.Info.Println("Task completed:", task.Todo)
-			return
 		}
 
-		// else, lets do it interactively?
-		tasks, err := todotxt.GetTasks()
+		// commit to git
+		err := git.CommitTodo()
 		if err != nil {
-			pExit("Error loading task list: ", err)
+			pExit("Error committing task completion: ", err)
 		}
-
-		// filter out completed tasks
-		tasks = tasks.Filter(todo.FilterNotCompleted)
-
-		var options []string
-		for _, task := range tasks {
-			options = append(options, strconv.Itoa(task.ID)+" | "+task.Todo)
-		}
-
-		selectedOptions, _ := pterm.DefaultInteractiveMultiselect.WithOptions(options).Show()
-
-		pterm.Info.Printfln("Selected options: %s", pterm.Green(selectedOptions))
 
 	},
 }
